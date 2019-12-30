aed5492c5da898410a02fc3c63a9dcebf2dde4e1
Fernando Zaragoza <fer.zm@outlook.com>
2019-12-29 17:16:26 -0800

    Add .ino file.

---
+++ Shell_BLDC/Shell_BLDC/Shell_BLDC.ino
@@ -0,0 +1,31 @@
+/*
+ Author:            Escuderia Borregos CCM.
+ Description:       ESP32 Firmware code for the Shell Eco-marathon 2020 @ Sonoma, CA.
+ Colaborators:      Israel Cayetano, Fernando Zaragoza, Angel Romero
+*/
+
+/******************************************************************
+ *                         I N C L U D E S                        *
+ ******************************************************************/
+
+#include "DRV8323H.h"                   // #include "PWM_ESP32.h" is included in the BLDC Driver header file.
+#include "ESP32_ExtInterrupt.h"
+
+ /*****************************************************************
+ *                  C L A S S   I N S T A N C E S                 *
+ ******************************************************************/
+
+
+
+ /*****************************************************************
+ *                         M A I N  L O O P S                      *
+ ******************************************************************/
+
+void setup() {
+
+}
+
+
+void loop() {
+
+}
